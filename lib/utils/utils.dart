d701aeb5551d2da4a974d11cd8c82ef0deff3fdb
mansisdev334 <mansisdev334@gmail.com>
2025-09-19 16:45:50 +0530

    code push

---
+++ lib/utils/utils.dart
@@ -0,0 +1,64 @@
+import 'package:delivermate/res/constant/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class Utils {
+  bool isValidName(String name) {
+    RegExp regExp = RegExp(r"^[a-zA-Z]{3,}");
+    return regExp.hasMatch(name);
+  }
+
+  bool isValidPassword(String contact) {
+    // RegExp regExp = RegExp(r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)");
+    RegExp regExp = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{6,}$');
+    return regExp.hasMatch(contact);
+  }
+
+  bool isValidMobile(String number) {
+    RegExp regExp = RegExp(r'(^(?:[+0]9)?[0-9]{10,12}$)');
+    return regExp.hasMatch(number);
+  }
+
+  bool isValidDateOfBirth(String number) {
+    RegExp regExp = RegExp(
+      r'^\d{2}-(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)-\d{4}$',
+    );
+    return regExp.hasMatch(number);
+  }
+
+  bool isValidEmail(String email) {
+    RegExp regExp = RegExp(
+      r'^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$',
+    );
+    return regExp.hasMatch(email);
+  }
+
+  void showSnackBar(
+    BuildContext context, {
+    String? message,
+    String? label,
+    SizedBox? sizeBox,
+    EdgeInsetsGeometry? margin,
+    void Function()? onPressed, //optional parameter
+  }) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        margin: margin!,
+        content: Row(
+          children: [
+            const CircularProgressIndicator(),
+            sizeBox!,
+            Text(message ?? ""),
+          ],
+        ),
+        backgroundColor: Colors.black,
+        duration: const Duration(seconds: 2),
+        behavior: SnackBarBehavior.floating,
+        action: SnackBarAction(
+          label: label ?? "",
+          onPressed: onPressed ?? () {},
+          textColor: AppColors.colorC8281E,
+        ),
+      ),
+    );
+  }
+}
